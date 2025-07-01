ee3c0f9af0c440c132d2dd94c4b39d2c774e5dd2
abrahanalarcon <moiseshazim@gmail.com>
2025-06-30 20:04:08 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Networking;
+
+
+namespace ManageOffline
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            VerificarConexion();
+        }
+
+        private void VerificarConexion()
+        {
+            var estado = Connectivity.Current.NetworkAccess;
+
+            if (estado != NetworkAccess.Internet)
+            {
+                lblEstadoConexion.Text = "Sin conexión a internet";
+                DisplayAlert("Advertencia", "No tienes conexión a internet", "OK");
+            }
+            else
+            {
+                lblEstadoConexion.Text = "Conectado a internet";
+            }
+        }
+
+        private void OnCheckConnectionClicked(object sender, EventArgs e)
+        {
+            VerificarConexion();
+        }
+    }
+
+}
